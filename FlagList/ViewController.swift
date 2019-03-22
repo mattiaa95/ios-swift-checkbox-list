138ee8ddfc0a494db809d817ad928b800c7b78f9
Mattia <mattia@mac-movilidad.local>
2019-03-22 11:12:27 +0000

    Initial Commit

---
+++ FlagList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlagList
+//
+//  Created by Mattia on 22/03/2019.
+//  Copyright © 2019 Mattia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
